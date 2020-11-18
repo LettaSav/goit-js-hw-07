32d8b841e7f714435e54c2c44a8219a74b90e2e1
LettaSav <lettawetta@icloud.com>
2020-11-18 21:21:45 +0200

    Update task_6.js

--- task_6.js
+++ task_6.js
@@ -1,11 +1,11 @@
 const inputRef = document.querySelector("#validation-input");
 inputRef.addEventListener("blur", colorChanger);
 function colorChanger() {
-  if (this.getAttribute("data-length") > this.value.length) {
-    this.classList.remove("valid");
-    this.classList.add("invalid");
-  } else {
+  if (this.getAttribute("data-length") === this.value.length) {
     this.classList.remove("invalid");
     this.classList.add("valid");
+  } else {
+    this.classList.remove("valid");
+    this.classList.add("invalid");
   }
 }
